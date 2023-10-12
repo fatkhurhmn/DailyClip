241c6f409832d7432771b207a2fcf89ebae5ec73
fatkhu rohman <fatkhurohman019@gmail.com>
2023-10-12 17:40:19 +0700

    add app logo

--- app/src/main/java/com/muffar/dailyclip/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/muffar/dailyclip/presentation/home/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.muffar.dailyclip.presentation.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -35,6 +39,16 @@ fun HomeScreen(
             .verticalScroll(rememberScrollState())
             .padding(vertical = 16.dp)
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.img_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         HomeSection(title = "") {
             if (nowPlayingState.isLoading) {
                 NowPlayingMoviesLoading()
